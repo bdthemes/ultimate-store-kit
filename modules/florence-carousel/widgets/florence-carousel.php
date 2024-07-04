19dba1abf7aba54bc0262b7ec18957be22c19bd3
Faridul Islam <faridbpi10@gmail.com>
2024-07-04 16:57:37 +0600

    florence carousel css optimized

--- modules/florence-carousel/widgets/florence-carousel.php
+++ modules/florence-carousel/widgets/florence-carousel.php
@@ -244,12 +244,13 @@ class Florence_Carousel extends Module_Base {
             <div class="usk-shoping">
                 <?php
                 $this->register_global_template_add_to_wishlist($tooltip_position);
+                $this->register_global_template_add_to_compare($tooltip_position);
                 $this->register_global_template_quick_view($product->get_id(), $tooltip_position);
                 $this->register_global_template_add_to_cart($tooltip_position);
                 ?>
             </div>
             <div class="usk-badge-label-wrapper">
-                <div class="usk-badge-label-content">
+                <div class="usk-badge-label-content usk-flex usk-flex-column">
                     <?php $this->register_global_template_badge_label(); ?>
                 </div>
             </div>
