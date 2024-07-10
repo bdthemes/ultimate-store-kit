e9431f4fbc12afb084e76f99f1a715f989eecefc
Shahidul Islam <bdkoder@gmail.com>
2024-07-10 12:00:25 +0600

    Project settings fixed

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
         sass: {
             dist: {
                 options: {
@@ -158,12 +160,45 @@ module.exports = function (grunt) {
                     spawn: false,
                 },
             }
-        }
+        },
+        // make a zipfile
+        compress: {
+            main: {
+                options: {
+                    archive: 'ultimate-store-kit.zip'
+                },
+                files: [{
+                    src: [
+                        'admin/**',
+                        'assets/css/**',
+                        'assets/fonts/**',
+                        'assets/images/**',
+                        'assets/js/**',
+                        'assets/vendor/**',
+                        'base/**',
+                        'includes/**',
+                        'languages/**',
+                        'modules/**',
+                        'traits/**',
+                        'loader.php',
+                        'readme.txt',
+                        'ultimate-store-kit.php',
+                    ],
+                    dest: 'ultimate-store-kit/'
+                },]
+            }
+        },
     });
 
     grunt.loadNpmTasks('grunt-rtlcss');
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks('grunt-terser');
     grunt.loadNpmTasks("grunt-sass");
-    grunt.registerTask('default', ['sass', 'terser', 'watch']);
+    grunt.loadNpmTasks('grunt-contrib-compress');
+
+    if (process.env.NODE_ENV === 'development') {
+        grunt.registerTask('default', ['sass', 'terser', 'watch']);
+    } else {
+        grunt.registerTask('default', ['sass', 'terser', 'compress']);
+    }
 };
