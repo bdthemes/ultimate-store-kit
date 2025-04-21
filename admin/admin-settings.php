8b3bab40f94a3184be897885741febbd9e7e2abc
Maudud Hossain <maudud.bdthemes@gmail.com>
2025-04-21 16:53:08 +0600

    Fixed: Error from elementor temporary catch

--- admin/admin-settings.php
+++ admin/admin-settings.php
@@ -236,7 +236,12 @@ class UltimateStoreKit_Admin_Settings
         if (class_exists('Elementor\Modules\Usage\Module')) {
 
             $module     = Module::instance();
-            $elements   = $module->get_formatted_usage('raw');
+            
+            $old_error_level = error_reporting();
+ 			error_reporting(E_ALL & ~E_WARNING); // Suppress warnings
+ 			$elements = $module->get_formatted_usage('raw');
+ 			error_reporting($old_error_level); // Restore
+
             $usk_widgets = self::get_usk_widgets_names();
 
             if (is_array($elements) || is_object($elements)) {
