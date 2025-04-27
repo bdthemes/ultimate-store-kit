c1cd5e71a31cc11d846795dc17d2ade739d145af
ABU TALIB <abutalib4590@gmail.com>
2025-04-27 11:47:07 +0600

    checkout page error issue fixed

--- modules/page-checkout/widgets/page-checkout.php
+++ modules/page-checkout/widgets/page-checkout.php
@@ -30,7 +30,7 @@ class Page_Checkout extends Module_Base {
     }
     public function show_in_panel() {
         return get_post_type() === 'usk-template-builder' || get_post_type() === 'elementor_library' || get_post_type() === 'product';
-	}
+    }
 
     public function get_keywords() {
         return ['cart', 'order', 'wc', 'checkout', 'page'];
@@ -49,7 +49,7 @@ class Page_Checkout extends Module_Base {
     }
 
     public function has_widget_inner_wrapper(): bool {
-        return ! \Elementor\Plugin::$instance->experiments->is_feature_active( 'e_optimized_markup' );
+        return ! \Elementor\Plugin::$instance->experiments->is_feature_active('e_optimized_markup');
     }
     protected function register_controls() {
 
@@ -1035,6 +1035,13 @@ class Page_Checkout extends Module_Base {
     <?php
     }
     public function render() {
+        // Initialize WC cart in editor
+        if (Plugin::instance()->editor->is_edit_mode() && function_exists('WC')) {
+            WC()->frontend_includes();
+            WC()->initialize_session();
+            WC()->initialize_cart();
+        }
+
     ?>
 
         <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
