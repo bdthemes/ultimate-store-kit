955919b5e257ab6b1fdfda744231987dd0ea1e3a
Faridul Islam <faridbpi10@gmail.com>
2025-04-28 18:16:34 +0600

    widgets fix

--- modules/up-sells/module.info.php
+++ modules/up-sells/module.info.php
@@ -2,7 +2,7 @@
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
 return [
-	'title'              => esc_html__('Up Sells', 'ultimate-store-kit'),
+	'title'              => esc_html__('Upsells', 'ultimate-store-kit'),
 	'required'           => true,
 	'default_activation' => true,
 	'has_style'			 => true,
