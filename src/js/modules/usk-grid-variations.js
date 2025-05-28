5d3305d9842aef6c1c1390daeca91fd6091e8088
ABU TALIB <abutalib4590@gmail.com>
2025-05-28 11:39:36 +0600

    action btn wraped

--- src/js/modules/usk-grid-variations.js
+++ src/js/modules/usk-grid-variations.js
@@ -50,11 +50,7 @@ class USKGridVariations {
     }
 
     // Add required buttons
-    this.addResetButton();
-
-    if (this.sequentialMode) {
-      this.addBackButton();
-    }
+    this.addActionButtons();
   }
 
   // Bind methods to maintain 'this' context
@@ -100,11 +96,14 @@ class USKGridVariations {
     );
   }
 
-  // Add reset button to the variations container
-  addResetButton() {
-    if (this.$container.find(".usk-reset-variations").length === 0) {
-      const $resetButton =
-        jQuery(`<button type="button" class="usk-reset-variations" aria-label="Reset">
+  // Add action buttons (reset and back) to the variations container
+  addActionButtons() {
+    if (this.$container.find(".usk-variation-action-btn-wrap").length === 0) {
+      // Create button wrapper
+      const $actionBtnWrap = jQuery('<div class="usk-variation-action-btn-wrap"></div>');
+
+      // Create reset button
+      const $resetButton = jQuery(`<button type="button" class="usk-reset-variations" aria-label="Reset">
     <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
       <path
         stroke="currentColor"
@@ -114,30 +113,31 @@ class USKGridVariations {
         d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"
       />
     </svg>
-  </button>
-`);
-      $resetButton.insertAfter(
-        this.$container.find(".usk-variation-group").last()
-      );
-      $resetButton.hide();
-    }
-  }
+  </button>`);
 
-  // Add back button for sequential variation selection
-  addBackButton() {
-    if (this.$container.find(".usk-back-variation").length === 0) {
-      const $backButton = jQuery(`
-  <button type="button" class="usk-back-variation" aria-label="Go Back">
+      // Add reset button to wrapper
+
+      // Add back button if in sequential mode
+      if (this.sequentialMode) {
+        const $backButton = jQuery(`<button type="button" class="usk-back-variation" aria-label="Go Back">
     <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
 </svg>
-  </button>
-`);
+  </button>`);
 
-      $backButton.insertAfter(
-        this.$container.find(".usk-variation-group").last()
-      );
-      $backButton.hide();
+        // Add back button to wrapper
+        $actionBtnWrap.append($backButton);
+        $actionBtnWrap.append($resetButton);
+
+        $backButton.hide();
+        $resetButton.hide();
+      }
+
+      // Add the wrapper after the last variation group
+      $actionBtnWrap.insertAfter(this.$container.find(".usk-variation-group").last());
+
+      // Hide reset button initially
+      $resetButton.hide();
     }
   }
 
