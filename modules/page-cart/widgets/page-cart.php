cdedda5a0007d57835d828d7f44e5f5b64beb5fa
Faridul Islam <faridbpi10@gmail.com>
2025-06-15 13:06:09 +0600

    fix cart page

--- modules/page-cart/widgets/page-cart.php
+++ modules/page-cart/widgets/page-cart.php
@@ -29,6 +29,10 @@ class Page_Cart extends Module_Base {
 		return [ 'ultimate-store-kit' ];
 	}
 
+	public function show_in_panel() {
+        return get_post_type() === 'usk-template-builder' || get_post_type() === 'elementor_library' || get_post_type() === 'product';
+    }
+
 	public function get_style_depends() {
 		if ( $this->usk_is_edit_mode() ) {
 			return [ 'usk-all-styles' ];
