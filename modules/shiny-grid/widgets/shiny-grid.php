e8d0c1d9af228bb223903ecb642256f21e02618b
usuf-bdt <usufbdt@gmail.com>
2025-12-01 13:00:33 +0600

    issues fixed

--- modules/shiny-grid/widgets/shiny-grid.php
+++ modules/shiny-grid/widgets/shiny-grid.php
@@ -121,12 +121,12 @@ class Shiny_Grid extends Module_Base
                 'tablet_default' => '2',
                 'mobile_default' => '1',
                 'options' => [
-                    '1' => '1',
-                    '2' => '2',
-                    '3' => '3',
-                    '4' => '4',
-                    '5' => '5',
-                    '6' => '6',
+                    '1' => esc_html__('1', 'ultimate-store-kit'),
+                    '2' => esc_html__('2', 'ultimate-store-kit'),
+                    '3' => esc_html__('3', 'ultimate-store-kit'),
+                    '4' => esc_html__('4', 'ultimate-store-kit'),
+                    '5' => esc_html__('5', 'ultimate-store-kit'),
+                    '6' => esc_html__('6', 'ultimate-store-kit'),
                 ],
                 'condition' => [
                     'layout_style' => 'grid',
@@ -149,12 +149,12 @@ class Shiny_Grid extends Module_Base
                 'tablet_default' => '2',
                 'mobile_default' => '1',
                 'options' => [
-                    '1' => '1',
-                    '2' => '2',
-                    '3' => '3',
-                    '4' => '4',
-                    '5' => '5',
-                    '6' => '6',
+                    '1' => esc_html__('1', 'ultimate-store-kit'),
+                    '2' => esc_html__('2', 'ultimate-store-kit'),
+                    '3' => esc_html__('3', 'ultimate-store-kit'),
+                    '4' => esc_html__('4', 'ultimate-store-kit'),
+                    '5' => esc_html__('5', 'ultimate-store-kit'),
+                    '6' => esc_html__('6', 'ultimate-store-kit'),
                 ],
                 'condition' => [
                     'layout_style' => 'list'
