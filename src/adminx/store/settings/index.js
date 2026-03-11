8fed63b017d2a71c53703518effed2bfba29ab33
ABU TALIB <abutalib4590@gmail.com>
2026-03-11 14:31:18 +0600

    remove store no needed at this time

--- src/adminx/store/settings/index.js
+++
@@ -1,9 +0,0 @@
-import { createReduxStore, register } from '@wordpress/data';
-
-const store = createReduxStore('ultimate-store-kit/settings', {
-    reducer: (state = {}, action) => {
-        return state;
-    },
-});
-
-register(store);
