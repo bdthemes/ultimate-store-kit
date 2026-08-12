a2ed6ddf313c2f9c462a0848b81356d7f77ddf95
Faridul Islam <73481902+bddevfarid@users.noreply.github.com>
2026-08-12 15:08:34 +0600

    Bump version to 3.0.10

--- ultimate-store-kit.php
+++ ultimate-store-kit.php
@@ -4,7 +4,7 @@
  * Plugin Name: Ultimate Store Kit
  * Plugin URI: https://bdthemes.com/ultimate-store-kit/
  * Description: Build online stores in WordPress with the powerful store builder addon for Elementor. Enjoy a wide range of customizations and easily build product grids, carousels, single product/page elements, checkouts and more.
- * Version: 3.0.9
+ * Version: 3.0.10
  * Author: BdThemes
  * Author URI: https://bdthemes.com/
  * Text Domain: ultimate-store-kit
@@ -23,7 +23,7 @@ if (file_exists($ultimate_store_kit_autoload_file)) {
 }
 
 // Some pre define value for easy use
-define('BDTUSK_VER', '3.0.9');
+define('BDTUSK_VER', '3.0.10');
 define('BDTUSK__FILE__', __FILE__);
 define('BDTUSK_PNAME', basename(dirname(BDTUSK__FILE__)));
 define('BDTUSK_PBNAME', plugin_basename(BDTUSK__FILE__));
