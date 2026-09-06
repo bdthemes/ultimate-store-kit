71cb213bc5b52cbb37cdb8f34254d1d9fb5c1954
Maudud Hossain <86344567+maudud-bdthemes@users.noreply.github.com>
2026-09-06 11:17:50 +0600

    Sales notification polished ongoing

--- src/admin/icons.js
+++ src/admin/icons.js
@@ -258,6 +258,13 @@ export const LiveVisitorCountIcon = ({ className = 'h-5 w-5' }) => (
 	</svg>
 );
 
+export const SalesNotificationIcon = ({ className = 'h-5 w-5' }) => (
+	<svg {...baseProps} className={className} viewBox="0 0 24 24">
+		<path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+		<path d="M13.73 21a2 2 0 0 1-3.46 0" />
+	</svg>
+);
+
 const UpsellCrossSellIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -289,5 +296,6 @@ export const adminIconMap = {
 	other: GridIcon,
 	info: InfoIcon,
 	'live-visitor-count': LiveVisitorCountIcon,
+	'sales-notification': SalesNotificationIcon,
 	'upsell-cross-sell': UpsellCrossSellIcon,
 };
